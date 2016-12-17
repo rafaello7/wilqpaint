48c7712f5956fca2a74dcd1622b5c55255f63748
rafaello7 <fatwildcat@gmail.com>
2016-12-17 12:24:49 +0100

    package version in about dialog take from config.h

--- src/aboutdialog.c
+++ src/aboutdialog.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include "aboutdialog.h"
+#include "config.h"
 
 void showAboutDialog(GtkWindow *owner)
 {
@@ -11,6 +12,7 @@ void showAboutDialog(GtkWindow *owner)
             "/org/rafaello7/wilqpaint/aboutdialog.ui");
     dialog = GTK_DIALOG(gtk_builder_get_object(builder, "aboutDialog"));
     g_object_unref(builder);
+    gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), PACKAGE_VERSION);
     gtk_window_set_transient_for(GTK_WINDOW(dialog), owner);
     gtk_dialog_run(dialog);
     gtk_widget_destroy(GTK_WIDGET(dialog));
