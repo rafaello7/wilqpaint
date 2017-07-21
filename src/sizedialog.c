da79053659db9523ff909796bd1d63a935f63581
rafaello7 <fatwildcat@gmail.com>
2017-07-21 12:18:28 +0200

    add *2 and /2 buttons to size dialog

--- src/sizedialog.c
+++ src/sizedialog.c
@@ -29,12 +29,37 @@ static void on_spinheight_changed(GtkSpinButton *spin, gpointer user_data)
     }
 }
 
+static void on_buttonx2_changed(GtkButton *button, gpointer user_data)
+{
+    gdouble val;
+
+	isAutoChange = TRUE;
+	val = gtk_spin_button_get_value(spinWidth);
+	gtk_spin_button_set_value(spinWidth, 2.0 * val);
+	val = gtk_spin_button_get_value(spinHeight);
+	gtk_spin_button_set_value(spinHeight, 2.0 * val);
+	isAutoChange = FALSE;
+}
+
+static void on_buttondiv2_changed(GtkButton *button, gpointer user_data)
+{
+    gdouble val;
+
+	isAutoChange = TRUE;
+	val = gtk_spin_button_get_value(spinWidth);
+	gtk_spin_button_set_value(spinWidth, 0.5 * val);
+	val = gtk_spin_button_get_value(spinHeight);
+	gtk_spin_button_set_value(spinHeight, 0.5 * val);
+	isAutoChange = FALSE;
+}
+
 gboolean showSizeDialog(GtkWindow *owner, const char *title,
         gdouble *width, gdouble *height, gboolean keepRatio)
 {
     GtkBuilder *builder;
     GtkDialog *dialog;
     gboolean result = FALSE;
+	GtkButton *buttonX2, *buttonDiv2;
 
     builder = gtk_builder_new_from_resource(
             "/org/rafaello7/wilqpaint/sizedialog.ui");
@@ -42,6 +67,8 @@ gboolean showSizeDialog(GtkWindow *owner, const char *title,
     gtk_window_set_title(GTK_WINDOW(dialog), title);
     spinWidth = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "spinWidth"));
     spinHeight = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "spinHeight"));
+    buttonX2 = GTK_BUTTON(gtk_builder_get_object(builder, "buttonX2"));
+    buttonDiv2 = GTK_BUTTON(gtk_builder_get_object(builder, "buttonDiv2"));
     g_object_unref(builder);
     gtk_spin_button_set_value(spinWidth, *width);
     gtk_spin_button_set_value(spinHeight, *height);
@@ -52,6 +79,10 @@ gboolean showSizeDialog(GtkWindow *owner, const char *title,
         g_signal_connect(G_OBJECT(spinHeight), "value-changed",
             G_CALLBACK(on_spinheight_changed), &ratio);
     }
+	g_signal_connect(G_OBJECT(buttonX2), "clicked",
+		G_CALLBACK(on_buttonx2_changed), NULL);
+	g_signal_connect(G_OBJECT(buttonDiv2), "clicked",
+		G_CALLBACK(on_buttondiv2_changed), NULL);
     gtk_window_set_transient_for(GTK_WINDOW(dialog), owner);
     if( gtk_dialog_run(dialog) == 1 ) {
         *width = gtk_spin_button_get_value(spinWidth);
